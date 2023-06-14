2d26e969cac93304842dcb15b59ea4eb61301867
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-14 19:16:06 +0530

    Added Hambargar || Made Responsive Header || SideBar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Header.css";
 import Nav from "../UI/Nav/Nav";
+import Hambargar from "../UI/Hambargar/Hambargar";
+import { ShowOnDesktop, ShowOnMobile } from "../../Styles/media";
 function Header() {
   return (
     <header className="Header">
+      <ShowOnMobile>
+        <i className="bx bx-menu"></i>
+        {/* <Hambargar /> */}
+      </ShowOnMobile>
       <h1>Rajesh Smp</h1>
-      <Nav />
+      <ShowOnDesktop>
+        <Nav />
+      </ShowOnDesktop>
     </header>
   );
 }
---
+++ src/Components/UI/Hambargar/Hambargar.jsx
@@ -0,0 +1,18 @@
+import "./Hambargar.css";
+function Hambargar() {
+  return (
+    <div className="Hambargar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Journey</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Hambargar;
